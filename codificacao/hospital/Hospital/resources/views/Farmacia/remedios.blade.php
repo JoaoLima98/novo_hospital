1264cfdb70a2023ea8d0209186d9cf5fd817fbe8
Douglas <jociandouglas12@gmail.com>
2026-01-31 00:12:25 -0300

    minimo de remedios para alerta #90

--- codificacao/hospital/Hospital/resources/views/Farmacia/remedios.blade.php
+++ codificacao/hospital/Hospital/resources/views/Farmacia/remedios.blade.php
@@ -6,17 +6,15 @@
 <a href="{{ route('remedios') }}" class="nav-item check"><i class="fas fa-pills"></i>Remédios</a>
 <a href="{{ route('consultar.estoque') }}" class="nav-item"><i class="fas fa-pills"></i> Estoque de remédios</a>
 <a href="{{ route('painel.guias') }}" class="nav-item"><i class="fas fa-calendar-check"></i> Entregar Medicamentos</a>
-
 @endsection
 
 @section('content')
 <div class="main-content">
     <div class="content-header">
         <h2 class="content-title"><i class="fas fa-calendar-check"></i> Remedios</h2>
-
     </div>
 
-   <div class="row">
+    <div class="row">
         @foreach($remedios as $remedio)
             <div class="col-md-3 mb-3">
                 <div class="card shadow-sm">
@@ -38,7 +36,6 @@
                             </div>
                         </div>
 
-
                     </div>
                 </div>
             </div>
@@ -46,7 +43,7 @@
     </div>
 </div>
 
-
+{{-- Scripts e Estilos --}}
 <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
 <link href="https://cdn.jsdelivr.net/npm/select2@4.1.0-rc.0/dist/css/select2.min.css" rel="stylesheet" />
 <script src="https://cdn.jsdelivr.net/npm/select2@4.1.0-rc.0/dist/js/select2.min.js"></script>
@@ -59,6 +56,21 @@
             let id = $(this).data('id');
             let valor = $(this).val();
 
+            // --- VALIDAÇÃO: Bloqueia se for menor que 5 ---
+            if (valor < 5) {
+                Swal.fire({
+                    icon: 'warning',
+                    title: 'Atenção',
+                    text: 'A quantidade de alerta não foi salva! O valor mínimo deve ser maior ou igual a 5.',
+                    confirmButtonColor: '#d33'
+                });
+                
+                $(this).val(5); // Força o valor mínimo de 5 no campo
+
+                return; 
+            }
+
+            
             $.ajax({
                 url: "/remedios-atualizar-alerta/" + id ,
                 type: "PUT",
@@ -76,7 +88,11 @@
                     });
                 },
                 error: function () {
-                    alert("Erro ao atualizar alerta");
+                    Swal.fire({
+                        icon: 'error',
+                        title: 'Erro',
+                        text: 'Erro ao conectar com o servidor.'
+                    });
                 }
             });
         });
@@ -84,4 +100,4 @@
     });
 </script>
 
-@endsection
+@endsection     
